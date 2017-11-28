2eacfb418a071c9b697516cf1c0da8cc29289cf4
chrisonntag <sonntagc@fim.uni-passau.de>
2017-11-28 13:20:41 +0100

    Check for settings.PRODUCTION in the controlboard class

--- controlboard.py
+++ controlboard.py
@@ -1,36 +1,42 @@
 from mmio.ex9055dm import EX9055DM
+from django.conf import settings
 
 
 class ControlBoard(EX9055DM):
     def __init__(self, address):
-        EX9055DM.__init__(self, address)
-        self.issues = {
-          'Pumpensumpf': 0,
-          'Rückstauklappe Hausmeister': 1,
-          'Rückstauklappe Technikraum': 2,
-          'Rauchmelder Allgemein B10': 3,
-          'Rauchmelder Allgemein L06': 4,
-          'Rauchmelder Allgemein L08': 5,
-          'Hitzemelder Tiefgarage': 6,
-          'Gasheizung': 7
-        }
+        if settings.PRODUCTION:
+            EX9055DM.__init__(self, address)
+            self.issues = {
+              'Pumpensumpf': 0,
+              'Rückstauklappe Hausmeister': 1,
+              'Rückstauklappe Technikraum': 2,
+              'Rauchmelder Allgemein B10': 3,
+              'Rauchmelder Allgemein L06': 4,
+              'Rauchmelder Allgemein L08': 5,
+              'Hitzemelder Tiefgarage': 6,
+              'Gasheizung': 7
+            }
 
     def check_issues(self):
-        found_issues = []
-        for k, v in self.issues.items():
-            input_status = self.read_input(v)
-            if input_status:
-                if input_status is 1:
-                    found_issues.append(k)
-            else:
-                raise ValueError('Could not read from the device')
+        if settings.PRODUCTION:
+            found_issues = []
+            for k, v in self.issues.items():
+                input_status = self.read_input(v)
+                if input_status:
+                    if input_status is 1:
+                        found_issues.append(k)
+                else:
+                    raise ValueError('Could not read from the device')
 
-        return found_issues
+            return found_issues
+        else:
+            return []
 
     def set_led(self, color):
-        if color is 'green':
-            self.write_output(1, 0)
-            self.write_output(0, 1)
-        elif color is 'red':
-            self.write_output(0, 0)
-            self.write_output(1, 1)
+        if settings.PRODUCTION:
+            if color is 'green':
+                self.write_output(1, 0)
+                self.write_output(0, 1)
+            elif color is 'red':
+                self.write_output(0, 0)
+                self.write_output(1, 1)
