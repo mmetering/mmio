254ef9f462a4154882f70de1fb1c4486b360e0af
Christoph Sonntag <info@chrisonntag.com>
2018-11-22 14:06:58 +0100

    Add logging to celery tasks

--- tasks.py
+++ tasks.py
@@ -1,11 +1,33 @@
+import logging
 from celery.schedules import crontab
 from celery.task import periodic_task
+from celery.signals import after_setup_task_logger
 from datetime import datetime, timedelta
 from mmio.handlers import supply_threshold_handler, check_input_pins_handler
 
-from celery.utils.log import get_task_logger
 
-logger = get_task_logger(__name__)
+def setup_logging(**kwargs):
+    """
+      Handler names is a list of handlers from your settings.py you want to
+      attach to this
+    """
+
+    handler_names = ['mail_admins', 'file', 'console']
+
+    import logging.config
+    from django.conf import settings
+    logging.config.dictConfig(settings.LOGGING)
+
+    logger = kwargs.get('logger')
+
+    handlers = [x for x in logging.root.handlers if x.name in handler_names]
+    for handler in handlers:
+        logger.addHandler(handler)
+        logger.setLevel(handler.level)
+        logger.propagate = False
+
+
+after_setup_task_logger.connect(setup_logging)
 
 
 @periodic_task(
@@ -14,6 +36,9 @@ logger = get_task_logger(__name__)
     ignore_result=True
 )
 def check_supply_task():
+    logger = check_supply_task.get_logger()
+    logger.setLevel(logging.DEBUG)
+
     supply_threshold_handler()
 
 
@@ -23,4 +48,7 @@ def check_supply_task():
     ignore_result=True
 )
 def check_input_pins_task():
+    logger = check_supply_task.get_logger()
+    logger.setLevel(logging.DEBUG)
+
     check_input_pins_handler()
